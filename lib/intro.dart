2e9302701c3dcbab4bf5de3aaefbb31f041f9acf
rpuiu <raz.puiu@gmail.com>
2020-04-26 23:10:07 +0300

    Fix Logo

--- lib/intro.dart
+++ lib/intro.dart
@@ -3,36 +3,43 @@ import 'package:carousel_slider/carousel_slider.dart';
 
 import 'home.dart';
 
-Color bgColor = Color(0xFFF3F3F3);
-Color textColor = Color(0xFF83838A);
-
 List<String> imagePath = [
   "assets/images/intro_pic.jpg",
   "assets/images/slide1.png",
   "assets/images/slide2.png",
   "assets/images/slide3.png",
 ];
-List<String> title = ["Welcome", "What you need", "Calibrate the sensor", "Scan your mask"];
+List<String> title = [
+  "Welcome",
+  "What you need",
+  "Calibrate the sensor",
+  "Scan your mask"
+];
 String welcomeString = "This app uses the optical sensor on the back of"
-"your phone to estimate the quality of your face mask.";
+    "your phone to estimate the quality of your face mask.";
 String page1String = "1. A dark or shaded area. \n"
-  "2. A hard, black surface with as little gloss as possible.\n"
-  "3. A Samsung Galaxy Note 4 (or newer) or Samsung Galaxy S6 (or newer).\n"
-  "Remove its case for best results.";
+    "2. A hard, black surface with as little gloss as possible.\n"
+    "3. A Samsung Galaxy Note 4 (or newer) or Samsung Galaxy S6 (or newer).\n"
+    "Remove its case for best results.";
 String page2String = "First calibrate the sensor by pressing the phone"
-"face down on the black surface. \n"
-"If calibration is not successful, the surface is too shiny, or you are"
-"not pressing down hard enough.";
-String page3String = "Once calibration is successful, place the mask in-between the phone and"
-"the black surface.\n"
-"Press the scan button and wait for the results";
-List<String> description = [welcomeString,page1String,page2String,page3String];
+    "face down on the black surface. \n"
+    "If calibration is not successful, the surface is too shiny, or you are"
+    "not pressing down hard enough.";
+String page3String =
+    "Once calibration is successful, place the mask in-between the phone and"
+    "the black surface.\n"
+    "Press the scan button and wait for the results";
+List<String> description = [
+  welcomeString,
+  page1String,
+  page2String,
+  page3String
+];
 
 class Intro extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: bgColor,
       body: ContentPage(),
     );
   }
@@ -72,7 +79,9 @@ class ContentPage extends StatelessWidget {
 
 class AppItro extends StatefulWidget {
   int index;
+
   AppItro(this.index);
+
   @override
   _AppItroState createState() => _AppItroState();
 }
@@ -95,13 +104,11 @@ class _AppItroState extends State<AppItro> {
           height: 50,
           child: Stack(
             children: <Widget>[
-              Positioned(
-                  left: 24,
-                  top: 14,
-                  child: new Text(
-                    "Numero\u00B9",
-                    style: TextStyle(fontFamily: "AvenirBold", fontSize: 16),
-                  )),
+              Image.asset(
+                "assets/images/logo.png",
+                fit: BoxFit.scaleDown,
+                width: 150,
+              ),
               Positioned(
                   right: 24,
                   top: 14,
@@ -113,43 +120,47 @@ class _AppItroState extends State<AppItro> {
                       widget.index == 3 ? "DONE" : "SKIP",
                       style: TextStyle(
                           fontSize: 16,
-                          color: textColor,
+                          color: Color(0xFF83838A),
                           fontFamily: "SultanNahia"),
                     ),
                   ))
             ],
           ),
         ),
-        Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height - 130,
-          child: Column(
-            children: <Widget>[
-              Image.asset(imagePath[widget.index],
-                  width: MediaQuery.of(context).size.width),
-              Center(
-                  child: new Text(
-                    title[widget.index],
-                    style: TextStyle(fontFamily: "Avenir Bold", fontSize: 30),
-                  )),
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 24, vertical: 24),
-                child: Center(
-                  child: new RichText(
+        Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+//            height: MediaQuery.of(context).size.height - 130,
+            child: Column(
+              children: <Widget>[
+                Image.asset(imagePath[widget.index],
+                    width: MediaQuery.of(context).size.width),
+                Center(
+                    child: new Text(
+                  title[widget.index],
+                  style: TextStyle(fontFamily: "Avenir Bold", fontSize: 30),
+                )),
+                Container(
+                  child: Center(
+                    child: new RichText(
                       textAlign: TextAlign.center,
                       text: new TextSpan(
-                          style: new TextStyle(
-                            fontSize: 14.0,
-                            color: Colors.black,
-                          ),
-                          children: <TextSpan>[
-                            new TextSpan(
-                                text: description[widget.index],
-                                style: new TextStyle(fontFamily: 'Avenir')),
-                          ])),
-                ),
-              )
-            ],
+                        style: new TextStyle(
+                          fontSize: 14.0,
+                          color: Colors.black,
+                        ),
+                        children: <TextSpan>[
+                          new TextSpan(
+                              text: description[widget.index],
+                              style: new TextStyle(fontFamily: 'Avenir')),
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
         Container(
@@ -168,13 +179,13 @@ class _AppItroState extends State<AppItro> {
                   top: widget.index != 3 ? 20 : 0,
                   child: widget.index != 3
                       ? Image.asset(
-                    'assets/images/arrow.png',
-                    width: 36,
-                  )
+                          'assets/images/arrow.png',
+                          width: 36,
+                        )
                       : LetsGo())
             ],
           ),
-        )
+        ),
       ],
     );
   }
@@ -182,7 +193,9 @@ class _AppItroState extends State<AppItro> {
 
 class Dots extends StatefulWidget {
   int index;
+
   Dots(this.index);
+
   @override
   _DotsState createState() => _DotsState();
 }
